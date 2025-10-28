9b44133630f52c2481bd224d465104e788521c72
olhapankiv <olha@pankiv.me>
2025-10-28 16:46:28 +0200

    updated favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import '../app.css'
-  import favicon from '$lib/assets/favicon.svg'
 
-  let { children } = $props()
+  import faviconico from '$lib/assets/favicon/favicon.ico'
+  import faviconsvg from '$lib/assets/favicon/favicon.svg'
+
+  const { children } = $props()
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
-
+  <link rel="icon" type="image/ico" href={faviconico} />
+  <link rel="icon" type="image/svg+xml" href={faviconsvg} />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
